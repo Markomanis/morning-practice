d9ab06e42cf0816fff18622e76ec7449ce5a2511
Marko Marciano <magicocoding@gmail.com>
2023-06-17 07:45:09 +0200

    in changes

---
+++ studying.py
@@ -0,0 +1,6 @@
+first_name = "eli"
+last_name = "hugo"
+full_name = f"{first_name.title()} {last_name.title()}"
+
+# print(f"Hello {first_name.title()} {last_name.title()}.")
+print("Lines order:\n\tFirst line \n\tSecond line\n\tThird line")
