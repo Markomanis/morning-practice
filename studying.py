0f38a6af196b95eaff84bd5760694e719d12f49a
Marko Marciano <magicocoding@gmail.com>
2023-06-17 08:28:37 +0200

    sc changes

--- studying.py
+++ studying.py
@@ -1,6 +1,18 @@
 first_name = "eli"
 last_name = "hugo"
-full_name = f"{first_name.title()} {last_name.title()}"
+full_name = f"{first_name.lower()} {last_name.upper()}"
 
 # print(f"Hello {first_name.title()} {last_name.title()}.")
-print("Lines order:\n\tFirst line \n\tSecond line\n\tThird line")
+# print("Lines order:\n\tFirst line \n\tSecond line\n\tThird line")
+# print(full_name)
+
+# space_name = ' \thugo\n\t'
+# print(space_name)
+# print(space_name.lstrip())
+# print(space_name.rstrip())
+# print(space_name.strip())
+
+
+file_name = "python_notes.txt"
+print(file_name)
+print(file_name.removesuffix(".txt"))
