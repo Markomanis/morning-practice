f710cdd508812dde58ceee96a2725f34cb973484
Marko Marciano <magicocoding@gmail.com>
2023-06-17 13:03:49 +0200

    Fancy Dinner

---
+++ lists.py
@@ -0,0 +1,55 @@
+# friends = ['friend_1', 'friend_2', 'friend_3']
+
+# print(f"Nice to finally see you {friends[0].title()}")
+# print(f"Nice to finally see you {friends[1].title()}")
+# print(f"Nice to finally see you {friends[2].title()}")
+
+fancy_dinner = ['Michael Flexson', 'George Looney', 'Tony Slark']
+
+print(f"Hello {fancy_dinner[0]}, you are invited to the fancy dinner.")
+print(f"Hello {fancy_dinner[1]}, you are invited to the fancy dinner.")
+print(f"Hello {fancy_dinner[2]}, you are invited to the fancy dinner.")
+
+print(f"\nUnfortunatelly {fancy_dinner[1]} can't make it to the fancy dinner.\n")
+
+del fancy_dinner[1]
+new_guest = "Nick Kindman"
+fancy_dinner.insert(1, new_guest) 
+
+print(f"Hello {fancy_dinner[0]}, you are invited to the fancy dinner.")
+print(f"Hello {fancy_dinner[1]}, you are invited to the fancy dinner.")
+print(f"Hello {fancy_dinner[2]}, you are invited to the fancy dinner.")
+
+print(f"\nHey you guys this is unreal, fcking crazy, the Ikea had this huge sale for table and now we got table with 3 extra seats!!!\n")
+
+new_guests = ['Patrick Puffy', 'Huge Jerkman', 'Kanye Sleeves']
+
+fancy_dinner.insert(0, new_guests[0])
+fancy_dinner.insert(1, new_guests[1])
+fancy_dinner.append(new_guests[2])
+
+print(f"Hello {fancy_dinner[0]}, you are invited to the fancy dinner.")
+print(f"Hello {fancy_dinner[1]}, you are invited to the fancy dinner.")
+print(f"Hello {fancy_dinner[2]}, you are invited to the fancy dinner.")
+print(f"Hello {fancy_dinner[3]}, you are invited to the fancy dinner.")
+print(f"Hello {fancy_dinner[4]}, you are invited to the fancy dinner.")
+print(f"Hello {fancy_dinner[5]}, you are invited to the fancy dinner.")
+
+print(f"\nOk so table won't make it on time and only 2 people can come to the Fancy Dinner.\n")
+
+popped_guest = fancy_dinner.pop(5)
+print(f"Sorry {popped_guest}, there is no room for you in the Fancy Dinner\n")
+popped_guest = fancy_dinner.pop(4)
+print(f"Sorry {popped_guest}, there is no room for you in the Fancy Dinner\n")
+popped_guest = fancy_dinner.pop(3)
+print(f"Sorry {popped_guest}, there is no room for you in the Fancy Dinner\n")
+popped_guest = fancy_dinner.pop(2)
+print(f"Sorry {popped_guest}, there is no room for you in the Fancy Dinner\n")
+
+print(f"Don't worry {fancy_dinner[0]}, you are still invited to the Fancy Dinner\n")
+print(f"Don't worry {fancy_dinner[1]}, you are still invited to the Fancy Dinner\n")
+
+del fancy_dinner[1]
+del fancy_dinner[0]
+print(fancy_dinner)
+
---
+++ numbers.py
@@ -0,0 +1,8 @@
+import this
+FAV_NUM = 14
+
+print(f"Your fav num is: {FAV_NUM} ")
+print(4+4)
+print(32/4)
+print(4*2)
+print(12-4)
