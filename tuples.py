e4efffa5746a4833cf29f790f2da16e29aea269a
Marko Marciano <magicocoding@gmail.com>
2023-06-18 07:10:57 +0200

    tuples

---
+++ tuples.py
@@ -0,0 +1,9 @@
+buffet = ('cheese', 'toast', 'eggs', 'parka', 'salami')
+
+for food in buffet:
+    print(food)
+
+buffet = ('cheese', 'futuristic-toast', 'futuristic-eggs', 'parka', 'salami')
+
+for food in buffet:
+    print(food)
