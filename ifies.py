154d340806813e943bbde9d4d29b6257d1c59608
Marko Marciano <magicocoding@gmail.com>
2023-06-18 08:19:02 +0200

    ifies around

--- ifies.py
+++ ifies.py
@@ -10,11 +10,50 @@
 #     else:
 #         print(f"Here is your license {name.title()}.")
 
-cars = ['toyota', 'BMW', 'subaru']
+# cars = ['toyota', 'BMW', 'subaru']
 
-for car in cars:
-    if car == 'Toyota'.lower():
-        print('We do have Toyota in the stock')
-    else: 
-        print("Toyota is out of stock")
-    break
+# for car in cars:
+#     if car == 'Toyota'.lower():
+#         print('We do have Toyota in the stock')
+#     else: 
+#         print("Toyota is out of stock")
+#     break
+
+# alien_color = 'water'
+
+# if alien_color == 'green':
+#     print("5 points scored!")
+# elif alien_color == 'red':
+#     print("10 points scored!")
+# else:
+#     print("15 points scored!")
+
+# age = 101
+
+# if age < 2:
+#     print("This is a baby we talking about.")
+# if (age > 2) and (age < 4):
+#     print("This is a todler we talking about.")
+# if (age >= 4) and (age < 13):
+#     print("This is a kid we talking about.")
+# if (age >= 13) and (age < 20):
+#     print("This is a teen we talking about.")
+# if (age >= 20) and (age < 65):
+#     print("This is an adult we talking about.")
+# if age > 65:
+#     print("This is an elder we talking about.")
+
+fav_fruits = ['banana', 'apple', 'orange']
+
+for fruit in fav_fruits:
+    if fruit == 'banana':
+        print("I like banana!")
+    if fruit == 'apple':
+        print("I like apple!")
+    if fruit == 'avocado':
+        print('whateva')
+    if fruit == 'ko':
+        print("I like banana!")
+    if fruit == 'orange':
+        print("I like orange!")
+    
