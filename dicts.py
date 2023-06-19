240118de92c1f93610e45d3028a18dfb50f46c87
Marko Marciano <magicocoding@gmail.com>
2023-06-19 12:52:59 +0200

    dictionares

--- dicts.py
+++ dicts.py
@@ -9,14 +9,50 @@
 
 # print(alien)
 
-person = {
-    'Susanne ' : ' 8',
-    'Danny ' : ' 10',
-    'Robin ' : ' 7',
-    'Kargo ' : ' 69',
-    'Petro ' : ' 22',
-    
-}
+# persons = {
+#     'Susanne ' : ' python',
+#     'Danny ' : ' c',
+#     'Robin ' : ' rust',
+#     'Kargo ' : ' java',
+#     'Petro ' : ''
+# }
 
-for x, y in person.items():
-    print(f"{x.lower()}:{y}")
+# for person, language in persons.items():
+#     if len(language) > 1:
+#         print(f"Thank you {person} for taking the poll")
+#     else:
+#         print(f"You should take the poll {person}")
+# for x, y in person.items():
+#     print(f"{x.lower()}:{y}")
+
+# rivers = {
+#     'nile' : 'egypt',
+#     'dunaj' : 'bratislava',
+#     'torysa' : 'presov'
+# }
+
+# for river in rivers.values():
+#     print(f"{river.title()}")
+
+# person_1 = {
+#     'name' : 'alex',
+#     'age' : '20',
+#     'city' : 'sobrance'
+# }
+# person_2 = {
+#     'name' : 'bruce',
+#     'age' : '11',
+#     'city' : 'gotham'
+# }
+# pet = {
+#     'name' : 'gofrey',
+#     'age' : '41',
+#     'city' : 'kebab'
+# }
+
+# people = [person_1, person_2, person_3]
+
+# for person in people:
+#     for key, value in person.items():
+#         print(f"{key} : {value}")
+
