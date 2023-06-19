d45f36b9d1ab576ce163e6f68d0799e59a2289e3
Marko Marciano <magicocoding@gmail.com>
2023-06-19 08:42:02 +0200

    dictionares

--- dicts.py
+++ dicts.py
@@ -1,10 +1,22 @@
-alien = {
-    'color':'green'
+# alien = {
+#     'color':'green'
+# }
+
+# print(alien['color'])
+
+# alien['x_pos'] = 0
+# alien['y_pos'] = 25
+
+# print(alien)
+
+person = {
+    'Susanne ' : ' 8',
+    'Danny ' : ' 10',
+    'Robin ' : ' 7',
+    'Kargo ' : ' 69',
+    'Petro ' : ' 22',
+    
 }
 
-print(alien['color'])
-
-alien['x_pos'] = 0
-alien['y_pos'] = 25
-
-print(alien)
+for x, y in person.items():
+    print(f"{x.lower()}:{y}")
