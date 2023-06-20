db3b9a381893870d6a9d42804260e7ea5b1f0817
Marko Marciano <magicocoding@gmail.com>
2023-06-20 07:36:06 +0200

    functioning

--- functions.py
+++ functions.py
@@ -20,29 +20,56 @@
 
 # city_country('Kosice', 'Slovakia')
 
-def make_album(artist_name, album_name, songs = None):
-    album = {'name' : artist_name, 'album' : album_name}
-    if songs:
-        album['songs'] = songs
-    print(album)
+# def make_album(artist_name, album_name, songs = None):
+#     album = {'name' : artist_name, 'album' : album_name}
+#     if songs:
+#         album['songs'] = songs
+#     print(album)
 
-while True:
-    print(f"Let's create album and artist. To quit anytime press 'q'. ")
+# while True:
+#     print(f"Let's create album and artist. To quit anytime press 'q'. ")
 
 
-    artist_name = input(f"Insert here artist name: ")
-    if artist_name == 'q':
-        break
+#     artist_name = input(f"Insert here artist name: ")
+#     if artist_name == 'q':
+#         break
     
-    album_name = input(f"Instert album name: ")
-    if album_name == 'q':
-        break
+#     album_name = input(f"Instert album name: ")
+#     if album_name == 'q':
+#         break
 
-    answer = input(f"Wish to save album? (y/n): ")
-    if answer == 'y':
-        break
-    if answer == 'n':
-        continue
+#     answer = input(f"Wish to save album? (y/n): ")
+#     if answer == 'y':
+#         break
+#     if answer == 'n':
+#         continue
 
-make_album(artist_name, album_name )  
+# make_album(artist_name, album_name )  
 
+# short_messages = ['yo', 'hey', 'booya']
+
+# def show_messages(messages):
+#     for message in short_messages:
+#         print(f"{message}")
+
+# show_messages(short_messages)
+
+
+old_messages = ['yo', 'hey', 'booya']
+new_messages = []
+
+print(f"Original Old list: {old_messages}")
+print(f"Original New list: {new_messages}")
+
+def send_message(old_mess, new_mess):
+    while old_mess:
+        message = old_mess.pop()
+        new_mess.append(message)
+
+    print(f"\nFunction Old list: {old_mess}.")
+    print(f"Function New list: {new_mess}.")
+
+send_message(old_messages[:], new_messages)
+
+print(f"\n Original Old list: {old_messages}")
+print(f"New list: {new_messages}")
