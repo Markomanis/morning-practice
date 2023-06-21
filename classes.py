fabe011ee42d0e8ceda0940fcd193635f2e0b79a
Marko Marciano <magicocoding@gmail.com>
2023-06-21 12:20:48 +0200

    class

--- classes.py
+++ classes.py
@@ -34,38 +34,46 @@
 
 # menu.flavours_display()
 
-class User():
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-        self.login_attempts = 0
+# class User():
+#     def __init__(self, first_name, last_name, age):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.age = age
+#         self.login_attempts = 0
 
-    def describe_user(self):
-        print(f"User name: {self.first_name}, {self.last_name}"
-              f" age of: {self.age}")
+#     def describe_user(self):
+#         print(f"User name: {self.first_name}, {self.last_name}"
+#               f" age of: {self.age}")
     
-    def greet_user(self):
-        print(f"Hello {self.first_name} {self.last_name}.")
+#     def greet_user(self):
+#         print(f"Hello {self.first_name} {self.last_name}.")
 
-    def increment_login_attempts(self):
-        self.login_attempts += 1
+#     def increment_login_attempts(self):
+#         self.login_attempts += 1
 
-    def reset_login_attempts(self):
-        self.login_attempts = 0
+#     def reset_login_attempts(self):
+#         self.login_attempts = 0
 
-class Admin(User):
-    def __init__(self, first_name, last_name, age, privileges):
-        super().__init__(first_name, last_name, age)
-        self.privileges = privileges
+# class Admin(User):
+#     def __init__(self, first_name, last_name, age, privileges):
+#         super().__init__(first_name, last_name, age)
+#         self.privileges = privileges
 
-    def show_privileges(self):
-        print(f"Admin's privileges: {self.privileges}")
+#     def show_privileges(self):
+#         print(f"Admin's privileges: {self.privileges}")
 
-admins_privileges = ['Can ban user', 'Can add user', 'Can remove user']
-admin = Admin('Peter', 'Veducko', 28, admins_privileges)
-admin.show_privileges()
-admin.describe_user()
+# admins_privileges = ['Can ban user', 'Can add user', 'Can remove user']
+# admin = Admin('Peter', 'Veducko', 28, admins_privileges)
+# admin.show_privileges()
+# admin.describe_user()
+
+# class Privileges():
+#     def __init__(self, privileges):
+#         self.privileges = privileges
+    
+#     def show_privileges(self):
+#         print(f"Admin's privileges: {self.privileges}")
+    
 
 # user_1 = User('Joe', 'Pesci', 20)
 # user_2 = User('Alfredo', 'Fetuccini', 41)
@@ -85,5 +93,4 @@ admin.describe_user()
 # user_1.reset_login_attempts()
 
 # print(f"User log in attempts resets to: {user_1.login_attempts}")
-
 
