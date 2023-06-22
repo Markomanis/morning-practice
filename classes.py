53ed2b6ea3f70c2573831fb3df28c5b113896520
Marko Marciano <magicocoding@gmail.com>
2023-06-22 07:31:45 +0200

    adding odometer to the car

--- classes.py
+++ classes.py
@@ -99,10 +99,15 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
     
     def get_descriptive_name(self):
         return (f"My car is: {self.make}, {self.model}, {self.year}".title())
+    
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
         
     
 my_new_car = Car('renault', 'clio', 2000)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
